b2cabd03448ba3bab317d549b1c3a0b5306b3365
Awsam Barahmeh <awsam.haitham@outlook.com>
2023-11-02 12:07:19 +0300

    Calculator logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,51 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const buttonPress = (symbol: string) => {
-    console.log(symbol)
+  const [answer, setAnswer] = useState("");
+  const [expression, setExpression] = useState("");
+  const et = expression.trim();
+  const isOperator = (symbol: string) =>{
+    return /[+/*-]/.test(symbol)
   }
+  const buttonPress = (symbol: string) => {
+    if(symbol === 'clear'){
+      setAnswer("0");
+      setExpression("");
+    } else if (symbol === "negative"){
+      if(answer === "") return;
+      setAnswer(
+        answer.toString().charAt(0) === "-" ? answer.slice(1) : "-" + answer
+      );
+    } else if (symbol === "percentage") {
+      if (answer === "") return;
+      setAnswer((parseFloat(answer) / 100).toString());
+    } else if (isOperator(symbol)){
+      setExpression (et + " " + symbol + " ");
+    } else if (symbol === "=") {
+      calculate();
+    } else if (symbol === "0") {
+      if(expression.charAt(0) !== "0") {
+        setExpression(expression + symbol)
+      }
+    } else if (symbol === "."){
+      // split by oprators and get last number
+      const lastNumber = expression.split(/[*+/-]/g).pop()
+      // if last number already has a decimal, don't add another
+      if(lastNumber?.includes(".")) return;
+      setExpression(expression + symbol)
+    }
+  };
+  const calculate = () => {
+
+  };
   return (
     <>
 <div className="container">
         <h1>Calculator Application</h1>
         <div id="calculator">
           <div id="display" style={{ textAlign: "right" }}>
-            <div id="answer">0</div>
-            <div id="expression"></div>
+            <div id="answer">{answer}</div>
+            <div id="expression">{expression}</div>
           </div>
           <button
             id="clear"
